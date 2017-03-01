0bda2fc772678d050820e7a7ad49955dc19e319d
zertu <290055513@qq.com>
2017-03-01 12:56:50 +0800

    project init

---
+++ ddl.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+
+const vendors = [
+    'react',
+    'react-dom',
+    'react-css-modules',
+    'mobx',
+    'mobx-react'
+]
+
+module.exports = {
+    output: {
+        path: 'dist',
+        filename: '[name].js',
+        library: '[name]',
+    },
+    entry: {
+        'lib': vendors,
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: 'manifest.json',
+            name: '[name]',
+            context: __dirname,
+        }),
+    ],
+}
